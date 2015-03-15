593d43622ccedf17acdf889a9bc8bd5ea1a8b01d
Cedric <cedric@adigit.com>
2015-03-15 18:26:02 +0100

    second commit
    exemples

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'vendor/autoload.php';
 use RedBean_Facade as r;
 
 // création de la relation base de donnée
-r::setup('mysql:host=localhost; dbname=', 'root', 'root');
+r::setup('mysql:host=localhost; dbname=nom de la base', 'root', 'root');
 r::freeze(true);
 
 
@@ -15,17 +15,61 @@ $app = new \Slim\Slim(array(
     'debug' => true
 ));
 
-// exemple d'utilisation de slim
-$app->get('/', function() use ($app) {
+/**
+ *  recupération de tous les exemples
+ */
+$app->get('/exemples', function() use ($app) {
 
-    // recupération des entité dans la base de donnée
-    $exemples = r::findAll('Nom de la base');
-    // je renvoie un header avec le type de fichier, la du json
+    $exemples = r::findAll('Nom de la table');
     $app->response()->header('Content-type','application/json');
-    // encodage des données en json
     echo json_encode(r::exportAll($exemples));
 
 });
+
+/**
+ * recuperation d'un exemple avec un id particulier et renvoie de la reponse sous forme de json
+ */
+$app->get('/exemples/:id', function($id) use ($app) {
+
+    try {
+        $exemples = r::findOne('nom de la table', 'id = ?', array($id));
+        if($exemples) {
+            $app->response()->header('content-type', 'application/json');
+            echo json_encode(r::exportAll($exemples));
+        }
+
+    } catch (Exception $e) {
+        $app->response()->status('404');
+        $app->response()->header('X-Status-Reason', $e->getMessage());
+    };
+
+});
+
+/**
+ * route de création d'un exemple
+ */
+$app->post('/exemples', function() use ($app) {
+
+    try {
+        $request = $app->request();
+        $body    = $request->getBody();
+        $input   = json_decode($body);
+
+        $exemples = r::dispense('nom de la base');
+
+        $exemples->titleExemple = $input->titleExemple;
+        $id = r::store($exemples);
+
+        $app->response()->header('content-type', 'application/json');
+        echo json_encode(r::exportAll($exemples));
+
+    } catch(Exception $e) {
+        $app->response()->status('400');
+        $app->response()->header('X-Status-Reason', $e->getMessage());
+    }
+
+});
+
 
 //lancement du serveur api
 $app->run();
